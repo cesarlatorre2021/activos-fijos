b49f652320d485923c0460f836bc632c7fe0d5f4
Cesar Latorre <sfecesar1941@hotmail.com>
2021-04-01 10:44:59 -0500

    Se adiciona clases que permite el manejo de las excepciones personalizadas

--- src/main/java/com/activos/fijos/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/activos/fijos/exception/CustomizedResponseEntityExceptionHandler.java
@@ -16,8 +16,8 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	@ExceptionHandler(BeanNotFoundException.class)
 	public final ResponseEntity<ExceptionResponse> handleNotFoundException(BeanNotFoundException ex, WebRequest request) {
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
-				request.getDescription(false),HttpStatus.NOT_ACCEPTABLE.getReasonPhrase());
-		return new ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.NOT_ACCEPTABLE);
+				request.getDescription(false),HttpStatus.NOT_FOUND.getReasonPhrase());
+		return new ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.NOT_FOUND);
 	}
 	
 }
